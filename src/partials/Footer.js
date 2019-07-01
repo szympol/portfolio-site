e2601fd2ca1902f793e19037fbd4a85dda4c51e0
Szymon Polanowski <szympol@hotmail.com>
2019-07-01 08:54:59 +0200

    Finish footer modal + RWD

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -28,8 +28,23 @@ class Footer extends React.Component {
       }
 
       rotateTooltip = (e) =>{
-        let a = document.querySelector('.tooltip')
-        a.classList.add('rotateTooltip');
+        let at = document.querySelector('.tooltip')
+        at.classList.add('rotateTooltip');
+      }
+
+      rotateTooltipGit = (e) =>{
+        let git = document.querySelector('#gitTooltip')
+        git.classList.add('rotateTooltip');
+      }
+
+      rotateTooltipLinkedin = (e) =>{
+        let linkedin = document.querySelector('#linkedinTooltip')
+        linkedin.classList.add('rotateTooltip');
+      }
+
+      rotateTooltipPhone = (e) =>{
+        let phone = document.querySelector('#phoneTooltip')
+        phone.classList.add('rotateTooltip');
       }
 
       render() {
@@ -45,23 +60,31 @@ class Footer extends React.Component {
             >
               <div className="footer__modal__info">
                 <div className="footer__modal__info--email" >
-                    <p data-tip data-for='happyFace'><FontAwesomeIcon icon={faAt} /></p>
-                    <ReactTooltip className='tooltip' id='happyFace' type='error' effect='solid' afterShow={ this.rotateTooltip } place='top'>
-  <span>szympol@hotmail.com</span>
-</ReactTooltip>
+                    <a href="mailto:szympol@hotmail.com?Subject=Hello" data-tip data-for='emailTooltip'><FontAwesomeIcon icon={faAt} /></a>
+                    <ReactTooltip className='tooltip' id='emailTooltip' effect='solid' afterShow={ this.rotateTooltip } place='top'>
+                      <span>szympol@hotmail.com</span>
+                    </ReactTooltip>
                     <div className="footer__modal__close" onClick={this.closeModal}><FontAwesomeIcon icon={faTimes} /></div>
                 </div>
                 <div className="footer__modal__info--github">
-                    <p><FontAwesomeIcon icon={faGithub} /></p>
+                    <a href="https://github.com/szympol" target="_blanket" data-tip data-for='gitTooltip'><FontAwesomeIcon icon={faGithub} /></a>
+                    <ReactTooltip id='gitTooltip' effect='solid' afterShow={ this.rotateTooltipGit } place='top'>
+                      <span>github.com/szympol</span>
+                    </ReactTooltip>
                 </div>
                 <div className="footer__modal__info--linkedin">
-                   <p><FontAwesomeIcon icon={faLinkedin} /></p>
+                   <a href="https://www.linkedin.com/in/szympol" target="_blanket" data-tip data-for='linkedinTooltip'><FontAwesomeIcon icon={faLinkedin} /></a>
+                   <ReactTooltip id='linkedinTooltip' effect='solid' afterShow={ this.rotateTooltipLinkedin } place='top'>
+                      <span>linkedin.com/in/szympol</span>
+                    </ReactTooltip>
                 </div>
-                <div className="footer__modal__info--phone">
-                    <p><FontAwesomeIcon icon={faMobileAlt} /></p>
+                <div className="footer__modal__info--phone" >
+                    <a href="tel:+48512399691" data-tip data-for='phoneTooltip'><FontAwesomeIcon icon={faMobileAlt} /></a>
+                    <ReactTooltip id='phoneTooltip' effect='solid' afterShow={ this.rotateTooltipPhone } place='top'>
+                      <span className="footer__modal__info--phone--number">+48 512-399-691</span>
+                    </ReactTooltip>
                 </div>
               </div>
-              
             </Modal>
           </footer>
         );
