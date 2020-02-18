56d3b6ede85dcc275d5d98066c070df49182aed3
Szymon Polanowski <szympol@hotmail.com>
2020-02-18 22:39:59 +0100

    Add new projects and update content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 import ReactPageScroller from "react-page-scroller";
-import MediaQuery from 'react-responsive';
+import MediaQuery from "react-responsive";
 
-import DataEn from './data/DataEn.json';
-import DataDe from './data/DataDe.json';
-import DataPl from './data/DataPl.json';
-import projectsDataEn from './data/projectsDataEn.json';
-import projectsDataDe from './data/projectsDataDe.json';
-import projectsDataPl from './data/projectsDataPl.json';
+import DataEn from "./data/DataEn.json";
+import DataDe from "./data/DataDe.json";
+import DataPl from "./data/DataPl.json";
+import projectsDataEn from "./data/projectsDataEn.json";
+import projectsDataDe from "./data/projectsDataDe.json";
+import projectsDataPl from "./data/projectsDataPl.json";
 
 import Hero from "./partials/Hero";
 import About from "./partials/About";
@@ -20,78 +20,95 @@ import Footer from "./partials/Footer";
 
 class App extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        currentPage: 1,
-        data: DataEn,
-        projectsData: projectsDataEn
-      };
-      this._pageScroller = null;
+    super(props);
+    this.state = {
+      currentPage: 1,
+      data: DataEn,
+      projectsData: projectsDataEn
+    };
+    this._pageScroller = null;
   }
 
-  pageOnChange = (number) => {
-      this.setState({currentPage: number});
+  pageOnChange = number => {
+    this.setState({ currentPage: number });
   };
 
-  goToPage = (eventKey) => {
-    console.log(eventKey);
+  goToPage = eventKey => {
     this._pageScroller.goToPage(eventKey);
   };
 
   switchLanguageToEnglish = () => {
-    console.log('switchLanguageToEnglish');
     this.setState({
       data: DataEn,
       projectsData: projectsDataEn
     });
-  }
+  };
 
   switchLanguageToGerman = () => {
-    console.log('switchLanguageToGerman');
     this.setState({
       data: DataDe,
       projectsData: projectsDataDe
     });
-  }
+  };
 
   switchLanguageToPolish = () => {
-    console.log('switchLanguageToPolish');
     this.setState({
       data: DataPl,
       projectsData: projectsDataPl
     });
-  }
+  };
 
   render() {
-
     return (
       <div className="wrapper">
         <MediaQuery minWidth={992}>
           <React.Fragment>
-            <ReactPageScroller ref={c => this._pageScroller = c} pageOnChange={this.pageOnChange}>
-            <Hero strings={this.state.data[0].subtitle} data={this.state.data[0]}/>
-            <About data={this.state.data[1]}/>
-            <PortfolioBox data={this.state.data[2]} projectsData={this.state.projectsData}/>
-            <Technologies data={this.state.data[3]}/>
+            <ReactPageScroller
+              ref={c => (this._pageScroller = c)}
+              pageOnChange={this.pageOnChange}
+            >
+              <Hero
+                strings={this.state.data[0].subtitle}
+                data={this.state.data[0]}
+              />
+              <About data={this.state.data[1]} />
+              <PortfolioBox
+                data={this.state.data[2]}
+                projectsData={this.state.projectsData}
+              />
+              <Technologies data={this.state.data[3]} />
             </ReactPageScroller>
-            <Navigation data={this.state.data[4]} goToPage={this.goToPage}/>
-            <Languages switchLanguageToGerman={this.switchLanguageToGerman} switchLanguageToEnglish={this.switchLanguageToEnglish} switchLanguageToPolish={this.switchLanguageToPolish}/>
-            <Footer data={this.state.data[5]}/>
-        </React.Fragment>
+            <Navigation data={this.state.data[4]} goToPage={this.goToPage} />
+            <Languages
+              switchLanguageToGerman={this.switchLanguageToGerman}
+              switchLanguageToEnglish={this.switchLanguageToEnglish}
+              switchLanguageToPolish={this.switchLanguageToPolish}
+            />
+            <Footer data={this.state.data[5]} />
+          </React.Fragment>
         </MediaQuery>
         <MediaQuery maxWidth={991}>
-          <Navigation data={this.state.data[4]}/>
-          <Hero strings={this.state.data[0].subtitle} data={this.state.data[0]}/>
-          <About data={this.state.data[1]}/>
-          <PortfolioBox data={this.state.data[2]} projectsData={this.state.projectsData}/>
-          <Technologies data={this.state.data[3]}/>
-          <Languages switchLanguageToGerman={this.switchLanguageToGerman} switchLanguageToEnglish={this.switchLanguageToEnglish} switchLanguageToPolish={this.switchLanguageToPolish}/>
-          <Footer data={this.state.data[5]}/>
+          <Navigation data={this.state.data[4]} />
+          <Hero
+            strings={this.state.data[0].subtitle}
+            data={this.state.data[0]}
+          />
+          <About data={this.state.data[1]} />
+          <PortfolioBox
+            data={this.state.data[2]}
+            projectsData={this.state.projectsData}
+          />
+          <Technologies data={this.state.data[3]} />
+          <Languages
+            switchLanguageToGerman={this.switchLanguageToGerman}
+            switchLanguageToEnglish={this.switchLanguageToEnglish}
+            switchLanguageToPolish={this.switchLanguageToPolish}
+          />
+          <Footer data={this.state.data[5]} />
         </MediaQuery>
       </div>
-      );
+    );
   }
 }
-
 
 export default App;
--- src/partials/PortfolioBox.js
+++ src/partials/PortfolioBox.js
@@ -1,84 +1,144 @@
 import React from "react";
-import PortfolioCards from './PortfolioCards';
+import PortfolioCards from "./PortfolioCards";
 
 class PortfolioBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            projectsData: this.props.projectsData,
-            filteredProjects: this.props.projectsData
-        };
-    }
-    componentDidUpdate(prevProps) {
-        console.log(prevProps.projectsData[0].desc)
-        if ( prevProps.projectsData[0].desc !==  this.props.projectsData[0].desc) {
-            this.setState({
-                projectsData: this.props.projectsData,
-                filteredProjects: this.props.projectsData
-              })
-        }
-    }
-
-
-    filterPortfolio = (e) => {
-        const portfolioFilterListElements = document.querySelectorAll('.portfolio__filter__list__element');
-        const filterValue = e.target.getAttribute('data-filter');
-
-        portfolioFilterListElements.forEach(element => element.classList.remove('portfolio__filter__list__element--active'));
-        e.target.classList.add('portfolio__filter__list__element--active');
-
-        this.setState({
-            filteredProjects: this.state.projectsData.filter(card => card.type.includes(`${filterValue}`))
-        });
-
-        let allPortfolioCards = document.querySelectorAll('.portfolio__card');
-        allPortfolioCards.forEach(element => element.classList.remove('portfolio__card--active'));
-
-        setTimeout(function(){
-            let allPortfolioCards = document.querySelectorAll('.portfolio__card');
-            allPortfolioCards.forEach(element => element.classList.add('portfolio__card--active'));
-          }, 1);
-}
-
-    render() {
-      return (
-        <section className="component" id="portfolio">
-            <div className="container portfolio">
-                <h1>{this.props.data.section}</h1>
-                <div className="portfolio__filter">
-                    <ul className="portfolio__filter__list">
-                        <li className="portfolio__filter__list__element portfolio__filter__list__element--active" data-filter="all" onClick={(e) => this.filterPortfolio(e)}>{this.props.data.filterNav[0]}</li>
-                        <li className="portfolio__filter__list__element" data-filter="react" onClick={(e) => this.filterPortfolio(e)}>{this.props.data.filterNav[1]}</li>
-                        <li className="portfolio__filter__list__element" data-filter="layout" onClick={(e) => this.filterPortfolio(e)}>{this.props.data.filterNav[2]}</li>
-                        <li className="portfolio__filter__list__element" data-filter="api" onClick={(e) => this.filterPortfolio(e)}>{this.props.data.filterNav[3]}</li>
-                        <li className="portfolio__filter__list__element" data-filter="vanillajs" onClick={(e) => this.filterPortfolio(e)}>{this.props.data.filterNav[4]}</li>
-                        <li className="portfolio__filter__list__element" data-filter="game" onClick={(e) => this.filterPortfolio(e)}>{this.props.data.filterNav[5]}</li>
-                        <li className="portfolio__filter__list__element" data-filter="node" onClick={(e) => this.filterPortfolio(e)}>{this.props.data.filterNav[6]}</li>
-                        <li className="portfolio__filter__list__element" data-filter="vue" onClick={(e) => this.filterPortfolio(e)}>{this.props.data.filterNav[7]}</li>
-                    </ul>
-                </div>
-                  <div className="portfolio__box" onScroll={(e) => e.stopPropagation()} onWheel={(e) => e.stopPropagation()}>
-                    <main className="portfolio__content row">
-                    {this.state.filteredProjects.map(data => (
-                        <PortfolioCards
-                        key={data.id}
-                        id={data.id}
-                        img={data.img}
-                        alt={data.alt}
-                        title={data.title}
-                        desc={data.desc}
-                        demo={data.demo}
-                        github={data.github}
-                        type={data.type}
-                        />
-                        ))}
-                    </main>
-                </div>
-            </div>
-        </section>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      projectsData: this.props.projectsData,
+      filteredProjects: this.props.projectsData
+    };
+  }
+  componentDidUpdate(prevProps) {
+    console.log(prevProps.projectsData[0].desc);
+    if (prevProps.projectsData[0].desc !== this.props.projectsData[0].desc) {
+      this.setState({
+        projectsData: this.props.projectsData,
+        filteredProjects: this.props.projectsData
+      });
     }
   }
 
+  filterPortfolio = e => {
+    const portfolioFilterListElements = document.querySelectorAll(
+      ".portfolio__filter__list__element"
+    );
+    const filterValue = e.target.getAttribute("data-filter");
+
+    portfolioFilterListElements.forEach(element =>
+      element.classList.remove("portfolio__filter__list__element--active")
+    );
+    e.target.classList.add("portfolio__filter__list__element--active");
+
+    this.setState({
+      filteredProjects: this.state.projectsData.filter(card =>
+        card.type.includes(`${filterValue}`)
+      )
+    });
+
+    let allPortfolioCards = document.querySelectorAll(".portfolio__card");
+    allPortfolioCards.forEach(element =>
+      element.classList.remove("portfolio__card--active")
+    );
+
+    setTimeout(function() {
+      let allPortfolioCards = document.querySelectorAll(".portfolio__card");
+      allPortfolioCards.forEach(element =>
+        element.classList.add("portfolio__card--active")
+      );
+    }, 1);
+  };
+
+  render() {
+    return (
+      <section className="component" id="portfolio">
+        <div className="container portfolio">
+          <h1>{this.props.data.section}</h1>
+          <div className="portfolio__filter">
+            <ul className="portfolio__filter__list">
+              <li
+                className="portfolio__filter__list__element portfolio__filter__list__element--active"
+                data-filter="all"
+                onClick={e => this.filterPortfolio(e)}
+              >
+                {this.props.data.filterNav[0]}
+              </li>
+              <li
+                className="portfolio__filter__list__element"
+                data-filter="vue"
+                onClick={e => this.filterPortfolio(e)}
+              >
+                {this.props.data.filterNav[1]}
+              </li>
+              <li
+                className="portfolio__filter__list__element"
+                data-filter="react"
+                onClick={e => this.filterPortfolio(e)}
+              >
+                {this.props.data.filterNav[2]}
+              </li>
+              <li
+                className="portfolio__filter__list__element"
+                data-filter="layout"
+                onClick={e => this.filterPortfolio(e)}
+              >
+                {this.props.data.filterNav[3]}
+              </li>
+              <li
+                className="portfolio__filter__list__element"
+                data-filter="api"
+                onClick={e => this.filterPortfolio(e)}
+              >
+                {this.props.data.filterNav[4]}
+              </li>
+              <li
+                className="portfolio__filter__list__element"
+                data-filter="vanillajs"
+                onClick={e => this.filterPortfolio(e)}
+              >
+                {this.props.data.filterNav[5]}
+              </li>
+              <li
+                className="portfolio__filter__list__element"
+                data-filter="game"
+                onClick={e => this.filterPortfolio(e)}
+              >
+                {this.props.data.filterNav[6]}
+              </li>
+              <li
+                className="portfolio__filter__list__element"
+                data-filter="node"
+                onClick={e => this.filterPortfolio(e)}
+              >
+                {this.props.data.filterNav[7]}
+              </li>
+            </ul>
+          </div>
+          <div
+            className="portfolio__box"
+            onScroll={e => e.stopPropagation()}
+            onWheel={e => e.stopPropagation()}
+          >
+            <main className="portfolio__content row">
+              {this.state.filteredProjects.map(data => (
+                <PortfolioCards
+                  key={data.id}
+                  id={data.id}
+                  img={data.img}
+                  alt={data.alt}
+                  title={data.title}
+                  desc={data.desc}
+                  demo={data.demo}
+                  github={data.github}
+                  type={data.type}
+                />
+              ))}
+            </main>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default PortfolioBox;
