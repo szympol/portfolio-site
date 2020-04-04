63bbb058cdb0aa90e252da2c0f6d4244a114dfc8
Szymon Polanowski <szympol@hotmail.com>
2020-04-04 14:09:06 +0200

    Change packages

--- src/partials/About.js
+++ src/partials/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Zoom from 'react-reveal/Zoom';
+import { Zoom } from "react-awesome-reveal";
 
 const About = (props) => {
   return (
@@ -8,13 +8,7 @@ const About = (props) => {
         <h1>{props.data.section}</h1>
         <Zoom>
           <div className="about__text-dashed">
-            <p>{props.data.p1}</p>{" "}
-            <p>
-            {props.data.p2}
-            </p>{" "}
-            <p>
-            {props.data.p3}
-            </p>{" "}
+            <p>{props.data.p1}</p> <p>{props.data.p2}</p> <p>{props.data.p3}</p>{" "}
             <p>{props.data.p4}</p>
           </div>
         </Zoom>
