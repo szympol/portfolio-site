2c02007016a67b02db60fd38794f6fe007df4a4f
Szymon Polanowski <szympol@hotmail.com>
2020-04-04 14:32:59 +0200

    Refactor about

--- src/partials/About.js
+++ src/partials/About.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Zoom } from "react-awesome-reveal";
 
 const About = (props) => {
+  const { section, p1, p2, p3, p4 } = props.data;
   return (
     <section className="component" id="about">
       <div className="container about">
-        <h1>{props.data.section}</h1>
+        <h1>{section}</h1>
         <Zoom>
           <div className="about__text-dashed">
-            <p>{props.data.p1}</p> <p>{props.data.p2}</p> <p>{props.data.p3}</p>{" "}
-            <p>{props.data.p4}</p>
+            <p>{p1}</p> <p>{p2}</p> <p>{p3}</p> <p>{p4}</p>
           </div>
         </Zoom>
       </div>
