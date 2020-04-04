c8e67d1f6019fda87e60712cd3278328d850828b
Szymon Polanowski <szympol@hotmail.com>
2020-04-04 14:57:48 +0200

    Fix displaying portfolio cards

--- src/partials/PortfolioCards.js
+++ src/partials/PortfolioCards.js
@@ -1,17 +1,33 @@
 import React from "react";
 
-const PortfolioCards = props => (
+const PortfolioCards = (props) => (
   <div className="portfolio__card" id={props.id}>
     <div className="portfolio__wrapper">
       <img className="portfolio__image" src={props.img} alt={props.alt} />
       <div className="portfolio__links">
-        <a className="portfolio__github" href={props.github ? props.github : null} style={{display: props.github ? 'inline' : 'none' }} alt={props.alt} target="_blanket">GitHub</a>
-        <a className="portfolio__demo" href={props.demo ? props.demo : null} style={{display: props.demo ? 'inline' : 'none' }} alt={props.demo} target="_blanket">demo</a>
+        <a
+          className="portfolio__github"
+          href={props.github ? props.github : null}
+          style={{ display: props.github ? "inline" : "none" }}
+          alt={props.alt}
+          target="_blanket"
+        >
+          GitHub
+        </a>
+        <a
+          className="portfolio__demo"
+          href={props.demo ? props.demo : null}
+          style={{ display: props.demo ? "inline" : "none" }}
+          alt={props.demo}
+          target="_blanket"
+        >
+          demo
+        </a>
       </div>
     </div>
     <div className="portfolio__text">
-      <h4>{props.title}</h4>
-      <p>{props.desc}</p>
+      <h4 className="portfolio__text-title">{props.title}</h4>
+      <p className="portfolio__text-description">{props.desc}</p>
     </div>
   </div>
 );
